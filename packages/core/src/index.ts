fc28f9be8535abb7985e89e25df9c1cf9c9c48a4
汪東陽 <michael19920327@gmail.com>
2022-12-28 02:53:00 +0800

    feat(core): init player class

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,16 @@
-console.log('hello core')
+class AnotherPlayer extends HTMLMediaElement {
+  mount(root: Element) {
+    if(root instanceof AnotherPlayer) return;
+
+    root.appendChild(root);
+  }
+}
+
+export function createPlayer() {
+  const elementName = 'another-player';
+  customElements.define(elementName, AnotherPlayer);
+
+  return document.createElement(elementName);
+}
+
+
