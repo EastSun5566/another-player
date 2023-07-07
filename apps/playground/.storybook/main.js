67b01132460356db5fc8660dc52fa442ccb930b8
Michael Wang <michael19920327@gmail.com>
2023-07-08 02:01:24 +0800

    fix: `lit-html` import issue

--- apps/playground/.storybook/main.js
+++ apps/playground/.storybook/main.js
@@ -1,11 +1,11 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
-  "framework": "@storybook/web-components"
-}
+  "stories": ["../stories/**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials"],
+  "framework": {
+    name: "@storybook/web-components-webpack5",
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
+};
--- apps/playground/stories/Button.ts
+++ apps/playground/stories/Button.ts
@@ -1,22 +1,26 @@
 /* eslint-disable import/prefer-default-export */
 /* eslint-disable import/no-extraneous-dependencies */
 import { html } from 'lit-html';
-import { styleMap } from 'lit-html/directives/style-map';
+// import { styleMap } from 'lit-html/directives/style-map';
 import './button.css';
 
 /**
  * Primary UI component for user interaction
  */
 export const Button = ({
-  primary, backgroundColor = null, size, label, onClick,
+  primary,
+  backgroundColor = null,
+  size,
+  label,
+  onClick,
 }) => {
   const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
 
+  // style=${styleMap({ backgroundColor })}
   return html`
     <button
       type="button"
       class=${['storybook-button', `storybook-button--${size || 'medium'}`, mode].join(' ')}
-      style=${styleMap({ backgroundColor })}
       @click=${onClick}
     >
       ${label}
