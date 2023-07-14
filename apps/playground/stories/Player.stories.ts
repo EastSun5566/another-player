91743b786a2879bd0ddfbd48b538b7a275c938f5
Michael Wang <michael19920327@gmail.com>
2023-07-14 22:28:05 +0800

    chore: remove unused stories

--- apps/playground/stories/Player.stories.ts
+++ apps/playground/stories/Player.stories.ts
@@ -2,21 +2,37 @@
 import { html } from 'lit-html';
 import { Meta, StoryFn } from '@storybook/web-components';
 
-import { createPlayer, PlayerElement, type PlayerOptions } from '@another-player/core';
+import {
+  createPlayer,
+  // PlayerElement,
+  type PlayerOptions,
+} from '@another-player/core';
 
-const VIDEO_URL = 'https://cdn.jsdelivr.net/npm/big-buck-bunny-1080p@0.0.6/video.mp4';
+// let player: ReturnType<typeof createPlayer>;
 
-const player = createPlayer({
-  src: VIDEO_URL,
-});
-
+const DEFAULT_VIDEO_URL = 'https://cdn.jsdelivr.net/npm/big-buck-bunny-1080p@0.0.6/video.mp4';
 export default {
   title: 'Another Player',
+  argTypes: {
+    src: { defaultValue: DEFAULT_VIDEO_URL },
+  },
 } as Meta<PlayerOptions>;
 
-export const AnotherPlayer: StoryFn<PlayerOptions> = () => html`<div id="player"></div>`;
+export const AnotherPlayer: StoryFn<PlayerOptions> = ({ src }) => {
+  createPlayer({ src });
 
-AnotherPlayer.play = () => {
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  player.mount(document.querySelector<PlayerElement>('#player')!);
+  return html`
+  <div id="player">
+    <another-player src="${src}"></another-player>
+  </div>
+  `;
 };
+
+AnotherPlayer.args = {
+  src: DEFAULT_VIDEO_URL,
+};
+
+// AnotherPlayer.play = () => {
+//   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+//   player.mount(document.querySelector<PlayerElement>('#player')!);
+// };
